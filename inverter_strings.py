59e07e70d9830158f2b38fdc60dcd935a48bba77
Lanner Silva <125982000+LannerSilva@users.noreply.github.com>
2023-03-29 23:15:04 -0300

    inverter_Strings

---
+++ inverter_strings.py
@@ -0,0 +1,14 @@
+# inverter_strings
+
+txt = 'exercicio feito para target inversao de Strings'
+print()
+print(txt)
+print(txt[::-1])
+
+def inverter(txt):
+    return txt[::-1]
+
+print('Ovo')
+print(inverter('Ovo'))
+print('A procura de um estagio')
+print(inverter('A procura de um estagio'))
